0853f07566ca271dc357316dc2768885426bff23
marcel1n <marcelo.desenvolvedor.contato@gmail.com>
2024-04-20 22:33:48 -0300

    Polindo, otimizando e desenvolvendo as funcionalidades finais da classe 'Quadras'

--- classes/quadra.ts
+++ classes/quadra.ts
@@ -18,6 +18,96 @@ export class Quadra {
         this.horariosReservados = []
     }
 
+
+    public reservar([horarioInicial, horarioFinal]: [string, string]){
+
+        this.horariosReservados.push(...this.criarHorarios([horarioInicial, horarioFinal]))
+
+        //Organiza a array em ordem do menor para o maior
+        this.horarios.sort((x, y) => x - y)
+    }
+
+
+    public deletarReserva([horarioInicial, horarioFinal]: [string, string]){
+
+        this.deletarHorarios([horarioInicial, horarioFinal], this.horariosReservados)
+    }
+
+
+    public criarIntervalo([horarioInicial, horarioFinal]: [string, string]){
+
+        this.deletarHorarios([horarioInicial, horarioFinal], this.horarios)
+    }
+
+
+    public recriarHorarios([horarioInicial, horarioFinal]: [string, string], recriar: boolean = true){
+
+        if (recriar){
+            this.horariosReservados = []
+        }
+
+        this.horarios = this.criarHorarios([horarioInicial, horarioFinal])
+    }
+
+
+    public resetarHorariosReservados(){
+        this.horariosReservados = []
+    }
+
+
+    public obterHorarios(formatar: boolean = true, filtrar: boolean = false): number[] | string[]{
+
+        let horarios: number[];
+
+        if(filtrar){
+
+            horarios = this.filtrarHorarios(this.horarios, this.horariosReservados)
+        } 
+        else {
+            horarios = [...this.horarios]
+        }
+        
+
+        if (formatar){
+
+            return this.formatarHorarios(horarios)
+        }
+
+        return horarios
+    }
+
+
+    public obterHorariosReservados(formatar: boolean = true): number[] | string[]{
+
+        if(formatar){
+            return this.formatarHorarios(this.horariosReservados)
+        }
+
+        return [...this.horariosReservados]
+    }
+
+
+    //Funções privadas, é aqui onde o filho chora e mãe não vê...
+
+
+    private deletarHorarios([horarioInicial, horarioFinal]: [string, string], horario: number[]){
+
+        this.validarHoras([horarioInicial, horarioFinal])
+
+        const converterParaMinutos = (total: number, valor: number, index: number) => {
+            return index % 2 == 0 ? total += valor * 60 : total += valor 
+        }
+
+        const horarioI: number = this.pegarHoras(horarioInicial).reduce(converterParaMinutos, 0)
+        const horarioF: number = this.pegarHoras(horarioFinal).reduce(converterParaMinutos, 0)
+
+        const horarioInicialIndex: number = this.horarios.indexOf(horarioI)
+        const horarioFinalIndex: number = this.horarios.indexOf(horarioF)
+
+        //Deleta todos os horários entre o Horário Inicial e o Horário Final
+        horario.splice(horarioInicialIndex, horarioFinalIndex - horarioInicialIndex)
+    }
+
     private criarHorarios([horarioInicial, horarioFinal]: [string, string]): number[]{
 
         this.validarHoras([horarioInicial, horarioFinal])
@@ -27,6 +117,12 @@ export class Quadra {
 
         const horarios: number[] = []
 
+        /*
+        Cria todos os horários entre o inicial e o final em minutos. ex:
+        12:00 e 14:30 gerariam: [720, 750, 780, 810, 840, 870].
+        12:00 horas em minutos são 720, 14:30 em minutos são 870...
+        */
+
         for (let i = horarioI[0]; i <= horarioF[0]; i++){
 
             horarios.push(i * 60)
@@ -34,6 +130,11 @@ export class Quadra {
 
         }
 
+        /*
+        O loop acima SEMPRE cria o horário e o horário + 30 minutos,
+        Então independente se o horarioInicial fosse 12:30, ainda seria
+        criado o horário 12:00, o código abaixo remove essas inconsistências.
+        */
         if (horarioI[1] == 30){
             horarios.shift()
         }
@@ -44,12 +145,14 @@ export class Quadra {
         return horarios
     }
 
+    //Transforma uma string em uma array de números. Ex: "12:30" vira [12, 30]
     private pegarHoras(str: string): number[]{
         const arr:string[] = str.split(':') 
 
         return arr.map((num) => parseInt(num))
     }
 
+    //Valida se as horas foram passadas no formato correto "hh:mm"
     private validarHoras(str: string[] | string){
 
         //Se for uma array de strings, usa recursão para verificar cada valor dentro
@@ -61,30 +164,23 @@ export class Quadra {
         }
 
         if (str.length != 5)
-            throw Error(`Horário inválido ${str}`)
+        throw Error(`Horário inválido ${str}`)
         else if (str[2] != ":")
-            throw Error(`Horário inválido ${str}`)
+        throw Error(`Horário inválido ${str}`)
 
         const [hora, minuto] = this.pegarHoras(str) as [number, number]
 
         if (hora >= 24 || hora < 0 || hora == undefined)
-            throw Error(`Hora inválida ${hora}`)
+        throw Error(`Hora inválida ${hora}`)
         else if (minuto > 60 || minuto < 0 || minuto == undefined)
-            throw Error(`Minuto inválido ${minuto}`)
+        throw Error(`Minuto inválido ${minuto}`)
     }
 
-    public obterHorarios(flag: string = 'f'): number[] | string[]{
+    //Formata a array de horários em minutos para uma array de strings de horas e minutos. 
+    //ex: [720, 750, 780] se transforma em ["12:00", "12:30", "13:00"]
+    private formatarHorarios(horarios: number[]): string[]{
 
-        if (flag != 'f' && flag != 'n'){
-
-            throw Error(`A flag ${flag} não existe`)
-        }
-        else if (flag == 'n'){
-
-            return this.horarios
-        }
-        
-        const horariosFormatados: string[] = this.horarios.map((item) => {
+        const horariosFormatados: string[] = horarios.map((item) => {
 
             const hora: number = Math.floor(item / 60)
             const minuto: number = item % 60
@@ -95,50 +191,20 @@ export class Quadra {
         return horariosFormatados
     }
 
-    public obterHorariosReservados(flag: string = 'f'): number[] | string[]{
+    //Filtra com complexidade linear, O(n)
+    private filtrarHorarios(horarios: number[], filtro: number[]): number[]{
 
-        if (flag != 'f' && flag != 'n'){
+        let i: number = 0;
 
-            throw Error(`A flag ${flag} não existe`)
-        }
-        else if (flag == 'n'){
+        return horarios.flatMap((item) => {
 
-            return this.horariosReservados
-        }
+            if (item == filtro[i]) return [] 
 
-        
-        const horariosFormatados: string[] = this.horariosReservados.map((item) => {
-
-            const hora: number = Math.floor(item / 60)
-            const minuto: number = item % 60
-
-            return this.adicionarZero(hora) + ':' + this.adicionarZero(minuto)
+            i++;
+            return [item]
         })
-
-        return horariosFormatados
     }
 
+    //Helper que adiciona um zero na frente do número se ele ser menor que 10
     private adicionarZero = (valor: number) => valor < 10 ? '0' + valor : valor.toString()
-
-    public criarIntervalo([horarioInicial, horarioFinal]: [string, string]){
-
-        this.validarHoras([horarioInicial, horarioFinal])
-
-        const converterParaMinutos = (total: number, valor: number, index: number) => {
-            return index % 2 == 0 ? total += valor * 60 : total += valor 
-        }
-        
-        const horarioI: number = this.pegarHoras(horarioInicial).reduce(converterParaMinutos, 0)
-        const horarioF: number = this.pegarHoras(horarioFinal).reduce(converterParaMinutos, 0)
-
-        const horarioInicialIndex: number = this.horarios.indexOf(horarioI)
-        const horarioFinalIndex: number = this.horarios.indexOf(horarioF)
-
-        this.horarios.splice(horarioInicialIndex, horarioFinalIndex - horarioInicialIndex)
-    }
-
-    public reservar([horarioInicial, horarioFinal]: [string, string]){
-
-        this.horariosReservados.push(...this.criarHorarios([horarioInicial, horarioFinal]))
-    }
 }
