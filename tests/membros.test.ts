b9b0dcaac97f24bab0cd476942a13e0ee43ad1c4
João Otávio Leites Matuszevski <joaoolmdev@gmail.com>
2024-04-23 18:56:14 -0300

    arrumando a o teste da classe membros

--- tests/membros.test.ts
+++ tests/membros.test.ts
@@ -4,8 +4,7 @@ import { beforeEach, it, expect, describe} from 'vitest'
 describe('Testando a classe Membros', ()=> {
     it("Após a criação da classe, deve-se ser retornando o nome que foi passado", ()=>{
 
-        const membro = new Membro("1", "cleber")
-        expect(membro.id).toBe('1')
+        const membro = new Membro("cleber")
         expect(membro.nome).toBe('cleber')
     })
 })
